5b7aab9438796bbd3170353a01368c3d1630b958
Vincent Agnano <vinyll@protonmail.com>
2023-09-20 23:56:47 +0200

    fixed node export and default lego export

--- lego.config.js
+++ lego.config.js
@@ -1,3 +1,4 @@
 export default {
-    preStyle: `@import url('/assets/css/main.css');`
+    preStyle: `@import url('/assets/css/main.css');`,
+    preScript: `import { api } from '/assets/js/core.js'`
 }
