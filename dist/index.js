9a1100d94b84bcfe8e01fc8a008f62af2e77c94e
Vincent Agnano <vinyll@protonmail.com>
2023-09-20 23:59:28 +0200

    emoved dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import XList from './x-list.js'
-customElements.define('x-list', XList)
-
-import XVideo from './x-video.js'
-customElements.define('x-video', XVideo)
-
-import XApp from './x-app.js'
-customElements.define('x-app', XApp)
