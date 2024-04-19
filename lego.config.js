8dacbc19159f46bc3d513ff67c3e2987bbe9cd0e
Vincent Agnano <vinyll@protonmail.com>
2024-04-19 18:48:45 +0200

    fixed background color

--- lego.config.js
+++ lego.config.js
@@ -1,5 +1,5 @@
 export default {
-    preStyle: `@import url(/"assets/css/main.css");`,
+    preStyle: `@import url("/assets/css/main.css");`,
     preScript: `
 import { api } from "/assets/js/core.js"
 import store from "/assets/js/store.js"
